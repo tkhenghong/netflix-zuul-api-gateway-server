6a6b790c65685f30f06d857f56e8754d41d57925
Teoh Kheng Hong <tkhenghong@gmail.com>
2020-02-27 08:13:46 +0800

    Initial setup of API Gateway is ready

--- src/main/java/com/in28minutes/microservices/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
+++ src/main/java/com/in28minutes/microservices/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
@@ -2,12 +2,17 @@ package com.in28minutes.microservices.netflixzuulapigatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+// Enable API Gateway
+@EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
 public class NetflixZuulApiGatewayServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NetflixZuulApiGatewayServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NetflixZuulApiGatewayServerApplication.class, args);
+    }
 
 }
