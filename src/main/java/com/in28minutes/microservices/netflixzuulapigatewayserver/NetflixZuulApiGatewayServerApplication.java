76a0881ccb02c7e506e38412d2f6d99b873ee980
Teoh Kheng Hong <tkhenghong@gmail.com>
2020-02-28 08:09:11 +0800

    Added Distributed Tracing with Spring Cloud Sleuth by Zipkin

--- src/main/java/com/in28minutes/microservices/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
+++ src/main/java/com/in28minutes/microservices/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
@@ -1,9 +1,11 @@
 package com.in28minutes.microservices.netflixzuulapigatewayserver;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 // Enable API Gateway
 @EnableZuulProxy
@@ -15,4 +17,15 @@ public class NetflixZuulApiGatewayServerApplication {
         SpringApplication.run(NetflixZuulApiGatewayServerApplication.class, args);
     }
 
+    // This is for the old Version
+//    @Bean
+//    public AlwaysSampler defaultSampler() {
+//        return new AlwaysSampler();
+//    }
+
+    // Trace all requests
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
 }
